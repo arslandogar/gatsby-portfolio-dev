2727c08959ca551022f9800ead193d349c17a99b
arslandogar <arslandogar112@yahoo.com>
2019-11-24 21:03:08 +0500

    changed config

--- src/data/config.js
+++ src/data/config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  defaultTitle: 'John Doe',
+  defaultTitle: 'Arslan Dogar',
   logo: 'https://portfolio.smakosh.com/favicon/favicon-512.png',
-  author: 'John Doe',
+  author: 'Arslan Dogar',
   url: 'https://portfolio.smakosh.com',
-  legalName: 'John Doe',
-  defaultDescription: 'I’m John and I’m a Backend & Devops engineer!',
+  legalName: 'Arslan',
+  defaultDescription: 'I’m Arslan and I’m a Backend & Devops engineer!',
   socialLinks: {
     twitter: 'http://www.twitter.com/smakosh',
     github: 'https://github.com/smakosh',
